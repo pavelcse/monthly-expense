fa4b5cb169d6e0fc82275ec5bae0d3c057325b72
Pavel Parvej <pavel.parvej@bjitgroup.com>
2021-08-11 15:04:02 +0600

    Issue fixing 3

--- server.js
+++ server.js
@@ -26,12 +26,13 @@ app.use('/api/users', userRouter);
 app.use('/api/items', itemRouter);
 app.use('/api/expenses', expenseRouter);
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+  
+    app.get('*', (request, response) => {
+      response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 app.get('/', (req, res) => {
     res.json({
